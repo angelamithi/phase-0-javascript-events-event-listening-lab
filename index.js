1b02696ccf6e411555f76927e5be037dd4f30615
+ <+>
2023-09-15 11:21:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert); // Move this line inside the function
 }
+
+// Call the function to set up the event listener
+addingEventListener();
